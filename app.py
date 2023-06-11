6494d1678df5f5e374d59f667639bc3d149f1c00
darshanasam1234 <64640091+darshanasam1234@users.noreply.github.com>
2023-06-11 23:53:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
 app = Flask(__name__)
 
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
-
-@app.route('/')
-@app.route('/hello')
-def hello():
-    # Render the page
-    return "Hello Python this is my first app!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        return f"Hello {name}! Your email address is {email}."
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Run the app server on localhost:4449
-    app.run('localhost', 4449)
+    app.run()
